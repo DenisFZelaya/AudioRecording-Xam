c2411b489540731d6c6c96c5fae325d6b5461b9f
Denis Federico Zelaya <48973732+DenisFZelaya@users.noreply.github.com>
2021-06-13 18:33:58 -0600

    Initial commit

---
+++ AudioRecording/AudioRecording/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Plugin.AudioRecorder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace AudioRecording
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly AudioRecorderService audioRecorderService = new AudioRecorderService();
+        private readonly AudioPlayer audioPlayer = new AudioPlayer();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var status = await Permissions.CheckStatusAsync<Permissions.Microphone>();
+
+            if(status != PermissionStatus.Granted)
+            {
+                return;
+            }
+
+            if (audioRecorderService.IsRecording)
+            {
+              await audioRecorderService.StopRecording();
+                audioPlayer.Play(audioRecorderService.GetAudioFilePath());
+            }
+            else
+            {
+                await audioRecorderService.StartRecording();
+            }
+        }
+    }
+}
